882680aec7df4f67ed569c27ed27ed63ea862a8a
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-02 14:25:32 -0700

    converting our HTML Content

--- react-version/src/components/TweetList.js
+++ react-version/src/components/TweetList.js
@@ -1,23 +1,31 @@
 import React from 'react';
 
 const TweetList = () => {
+  const tweet = {
+    avatar: "https://i.imgur.com/73hZDYK.png",
+    firstName: "Isaac",
+    lastName: "Newton",
+    handle: "@SirIsaac",
+    body: "If I have seen further it is by standing on the shoulders of giants",
+    age: "10 days ago"
+  }
   return (
     <section className="tweets">
 
       <article className="tweet">
 
         <header className="tweet--header">
-          <img className="tweet--avatar" src="https://i.imgur.com/73hZDYK.png" />
-          <h2 className="tweet--name">Newton</h2>
-          <small className="tweet--handle">@SirIsaac</small>
+          <img className="tweet--avatar" src={tweet.avatar} />
+          <h2 className="tweet--name">{tweet.lastName}</h2>
+          <small className="tweet--handle">{tweet.handle}</small>
         </header>
 
         <div className="tweet--body">
-          <p>If I have seen further it is by standing on the shoulders of giants</p>
+          <p>{tweet.body}</p>
         </div>
 
         <footer className="tweet--footer">
-          <small className="footer--age">10 days ago
+          <small className="footer--age">{tweet.age}
             <small>
               <span className="footer--actions">
                 <a href="#"><i className="fa fa-flag" /></a>
@@ -33,17 +41,17 @@ const TweetList = () => {
       <article className="tweet">
 
         <header className="tweet--header">
-          <img className="tweet--avatar" src="https://i.imgur.com/nlhLi3I.png" />
-          <h2 className="tweet--name">Descartes</h2>
-          <small className="tweet--handle">@rd</small>
+          <img className="tweet--avatar" src={tweet.avatar} />
+          <h2 className="tweet--name">{tweet.lastName}</h2>
+          <small className="tweet--handle">{tweet.handle}</small>
         </header>
 
         <div className="tweet--body">
-          <p>Je pense , donc je suis</p>
+          <p>{tweet.body}</p>
         </div>
 
         <footer className="tweet--footer">
-          <small className="footer--age">10 days ago
+          <small className="footer--age">{tweet.age}
             <small>
               <span className="footer--actions">
                 <a href="#"><i className="fa fa-flag" /></a>
@@ -53,7 +61,7 @@ const TweetList = () => {
             </small>
           </small>
         </footer>
-
+        
       </article>
 
     </section>
